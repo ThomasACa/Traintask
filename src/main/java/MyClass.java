8b96ffdcdd1d43647f962b85d82d701ed4dfae8e
ACojocari <Artiom.Cojocari@endava.com>
2020-10-29 18:08:30 +0200

    mandatory88888888 ggg hhhh

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -1,2 +1,7 @@
 public class MyClass {
+    public static void main(String[] args) {
+        System.out.println("Branch one");
+        System.out.println("Second change");
+        System.out.println("Test change");
+    }
 }
